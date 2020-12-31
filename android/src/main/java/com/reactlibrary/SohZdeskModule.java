29d74836c3a2c7020fbb699f20556cb8d879b822
Robert Cook <robert.cook@swbusinessconsultants.com>
2020-12-31 16:56:42 +0100

    android fix

--- android/src/main/java/com/reactlibrary/SohZdeskModule.java
+++ android/src/main/java/com/reactlibrary/SohZdeskModule.java
@@ -41,7 +41,6 @@ public class SohZdeskModule extends ReactContextBaseJavaModule {
                 .withEmailIdentifier(email)
                 .build();
         Zendesk.INSTANCE.setIdentity(identity);
-        Zendesk.INSTANCE.provider().pushRegistrationProvider().registerWithDeviceIdentifier(result, new ZendeskCallback<String>() {});
     }
     @ReactMethod
     public void openHelpCenter() {
